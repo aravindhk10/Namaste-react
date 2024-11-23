26a88d087ca6bb440f4ebac1695706047dfec7c8
Aravind H Krishnan <you@example.com>
2024-11-23 18:16:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},[React.createElement("h1",{id:"child-content"},"hello world inside child"),
+        React.createElement("h1",{id:"child-content1"},"hello world inside child1"),
+        React.createElement("h1",{id:"child-content2"},"hello world inside child2")
+    ]
+    
+    ),
+    React.createElement("div",{id:"child1"},React.createElement("h1",{},"hello world from inside new chalid within the parent"))
+]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+console.log(parent)
